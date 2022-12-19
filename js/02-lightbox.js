782a1b658acbfc5ea04b7e94bf117c256c10da41
ArtemKotlar <artemkotlar93@gmail.com>
2022-12-19 16:13:33 +0200

    task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+
+const galleryMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery">
+        <a class="gallery__item" href = ${original}>
+            <img 
+                class = "gallery__image"
+                src = ${preview} 
+                alt = ${description}
+            />
+        </a>
+    </li>`;
+  })
+  .join("");
+
+galleryRef.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+const lightbox = new SimpleLightbox(".gallery__item", {
+  captionSelector: "img",
+  captionType: "attr",
+  captionsData: "alt",
+  captionDelay: 250,
+});
